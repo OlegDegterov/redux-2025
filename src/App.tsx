ac0d938ea581038142f8b4c592acc47b9c7b18ae
Oleg Degterov <olegdegterov@gmail.com>
2025-07-13 17:50:48 +0300

    add record counters in store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { store, type DecrementAction, type IncrementAction } from "./store";
+import {
+  store,
+  type CounterId,
+  type DecrementAction,
+  type IncrementAction,
+} from "./store";
 import { useEffect, useReducer } from "react";
 
 function App() {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
-
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => forceUpdate());
-
-    return unsubscribe;
-  }, []);
   return (
     <>
       <div>
@@ -22,32 +20,54 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{store.getState().counter}</h1>
-      <div className="card">
-        <button
-          onClick={() =>
-            store.dispatch({ type: "increment" } satisfies IncrementAction)
-          }
-        >
-          increment
-        </button>
-        <button
-          onClick={() =>
-            store.dispatch({
-              type: "decrement",
-            } satisfies DecrementAction)
-          }
-        >
-          decrement
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <Counter counterId="first" />
+      <Counter counterId="second" />
+      <Counter counterId="third" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
+  );
+}
+
+export function Counter({ counterId }: { counterId: CounterId }) {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => forceUpdate());
+
+    return unsubscribe;
+  }, []);
+  return (
+    <div className="card">
+      <p>{counterId}</p>
+      <p>{store.getState().counters[counterId]?.counter}</p>
+
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: "increment",
+            payload: {
+              counterId,
+            },
+          } satisfies IncrementAction)
+        }
+      >
+        increment
+      </button>
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: "decrement",
+            payload: {
+              counterId,
+            },
+          } satisfies DecrementAction)
+        }
+      >
+        decrement
+      </button>
+    </div>
   );
 }
 
